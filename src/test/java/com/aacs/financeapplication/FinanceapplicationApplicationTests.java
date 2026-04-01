e6e783c4967a56e7f753c5321868f6d103c84c3e
augustocaixeta <augusto.alex@estudante.iftm.edu.br>
2026-03-31 21:26:24 -0300

    init: prepara ambiente Spring Boot

---
+++ src/test/java/com/aacs/financeapplication/FinanceapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aacs.financeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

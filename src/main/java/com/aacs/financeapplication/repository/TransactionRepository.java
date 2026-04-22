61267c2e04788d9eec9ff0ae8b3fe3dd6714aa8e
augustocaixeta <augusto.alex@estudante.iftm.edu.br>
2026-04-21 22:04:10 -0300

    Adiciona repository e service para entidade Transaction

---
+++ src/main/java/com/aacs/financeapplication/repository/TransactionRepository.java
@@ -0,0 +1,17 @@
+package com.aacs.financeapplication.repository;
+
+import com.aacs.financeapplication.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByType(String type);
+
+    List<Transaction> findByCategory(String category);
+
+    List<Transaction> findAllByOrderByDateDesc();
+}
